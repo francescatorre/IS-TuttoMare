9054513202aa4a5e4d29c7e5339e21cac9d47561
francescatorre <32592954+francescatorre@users.noreply.github.com>
2018-02-08 19:30:49 +0100

    Update TestDataBase.java

--- codice/src/model/TestDataBase.java
+++ codice/src/model/TestDataBase.java
@@ -1,5 +1,30 @@
 package model;
 
-public class TestDataBase {
 
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+public class TestDataBase {
+	@Test
+	public void testGetConnection() {
+		try {
+			assertNotEquals(DriverManagerConnectionPool.getConnection());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private void assertNotEquals(Connection connection) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void TestReleaseConnection(){
+		assertEquals(true,true);
+	}
 }
